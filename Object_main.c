52ab871d154d3412d514f5894d37acfe3169fd5d
matrixton02 <yashasvi21022005@gmail.com>
2025-03-07 00:18:52 +0530

    minor updates

--- Object_main.c
+++ Object_main.c
@@ -13,6 +13,9 @@ int main(){
     Object_t* c1=new_object_Complex(3.0f,4.0f);
     Object_t* c2=new_object_Complex(5.0f,3.0f);
     Object_t* c3=add(c1,c2);
+    printf("%d\n",intObj->data.v_int);
+    printf("%.2f\n",floatObj->data.v_float);
+    printf("%s\n",strObj->data.v_string);
     printf("%.2f %.2f \n",c3->data.v_complex.real,c3->data.v_complex.imag);
     
     object_free(intObj);
