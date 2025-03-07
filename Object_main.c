0514329a2e80f383da340be965df4bda8100dfe7
matrixton02 <yashasvi21022005@gmail.com>
2025-03-07 09:21:36 +0530

    added comments

--- Object_main.c
+++ Object_main.c
@@ -5,19 +5,22 @@
 
 
 int main(){
-    Object_t* intObj=new_object_Integer(5);
-    Object_t* intObj2=new_object_Integer(57);
-    Object_t* floatObj=new_object_Float(76.5f);
-    char* s="hello";
-    Object_t* strObj=new_object_String(s);
-    Object_t* c1=new_object_Complex(3.0f,4.0f);
+    Object_t* intObj=new_object_Integer(5); //Stored an Inetger in out Object
+    Object_t* intObj2=new_object_Integer(57); 
+    Object_t* floatObj=new_object_Float(76.5f); //Stored a Float in out Object
+    Object_t* strObj=new_object_String("Hello"); //Stored a string in our Object
+    Object_t* c1=new_object_Complex(3.0f,4.0f); //Stored a Complex number in our Object
     Object_t* c2=new_object_Complex(5.0f,3.0f);
-    Object_t* c3=add(c1,c2);
+    Object_t* c3=add(c1,c2); // A new Object with complex number is generated using add function.
+    Object_t* newStr=multiply(strObj,intObj); //Showcasing multiply fucntion with String and an Integer Object
+    //Printing different objects values
     printf("%d\n",intObj->data.v_int);
     printf("%.2f\n",floatObj->data.v_float);
     printf("%s\n",strObj->data.v_string);
+    printf("%s\n",newStr->data.v_string);
     printf("%.2f %.2f \n",c3->data.v_complex.real,c3->data.v_complex.imag);
     
+    //Freeing all the objects
     object_free(intObj);
     object_free(intObj2);
     object_free(floatObj);
